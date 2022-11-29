5355a579cea016a23b62989da0917d62ba28ec45
mateussc2000 <mateussc2000@gmail.com>
2022-11-29 14:06:16 -0300

    first commit

---
+++ javaecs/src/main/java/com/ecs/javaecs/JavaecsApplication.java
@@ -0,0 +1,13 @@
+package com.ecs.javaecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaecsApplication.class, args);
+	}
+
+}
---
+++ javaecs/src/test/java/com/ecs/javaecs/JavaecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecs.javaecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaecsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
