c5fd1842b7376f4123ee85d8eb08a0dd531dce3a
mateussc2000 <mateussc2000@gmail.com>
2022-11-29 16:54:44 -0300

    feat: added BankAccount model

--- javaecs/src/main/java/com/ecs/javaecs/model/BankAccount.java
+++ javaecs/src/main/java/com/ecs/javaecs/model/BankAccount.java
@@ -1,5 +1,40 @@
 package com.ecs.javaecs.model;
 
-public class BankAccount {
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "bank_account")
+public class BankAccount {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id", nullable = false)
+    private Long accountId;
+
+    @Column(name = "account_type", nullable = false)
+    private String accountType;
+
+    @Column(name = "account_balance", nullable = false)
+    private double balance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", referencedColumnName = "client_id")
+    @JsonBackReference
+    private Client client;
 }
