8557d95c7fa7e4c5ef799a94b02947858f81e1fd
mateussc2000 <mateussc2000@gmail.com>
2022-11-29 22:08:39 -0300

    build: Added ModelMapper dependecy
    - Configured ModelMapper Class as a Spring Bean

--- javaecs/src/main/java/com/ecs/javaecs/JavaecsApplication.java
+++ javaecs/src/main/java/com/ecs/javaecs/JavaecsApplication.java
@@ -1,11 +1,18 @@
 package com.ecs.javaecs;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JavaecsApplication {
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaecsApplication.class, args);
 	}
